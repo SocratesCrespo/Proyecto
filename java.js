661ac90c676e8f5ee0bf727d4fe0a719ff7f164b
SocratesCrespo <socratescrespo@gmail.com>
2014-07-03 21:39:35 -0430

    Cambio al 3 de Julio
    Sólo cositas simples como color de los nav.

---
+++ java.js
@@ -0,0 +1,254 @@
+var contador= 0;
+function one(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
+    document.botones1.boton1.disabled=true
+       document.botones1.boton2.disabled=true
+        document.botones1.boton3.disabled=true
+         document.botones1.boton4.disabled=true
+}
+
+function two(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
+   document.botones1.boton1.disabled=true
+       document.botones1.boton2.disabled=true
+        document.botones1.boton3.disabled=true
+         document.botones1.boton4.disabled=true
+}
+function one2(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
+    document.botones2.boton1.disabled=true
+       document.botones2.boton2.disabled=true
+        document.botones2.boton3.disabled=true
+         document.botones2.boton4.disabled=true
+}
+
+function two2(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
+   document.botones2.boton1.disabled=true
+       document.botones2.boton2.disabled=true
+        document.botones2.boton3.disabled=true
+         document.botones2.boton4.disabled=true
+}
+function one3(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
+    document.botones3.boton1.disabled=true
+       document.botones3.boton2.disabled=true
+        document.botones3.boton3.disabled=true
+         document.botones3.boton4.disabled=true
+}
+
+function two3(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
+   document.botones3.boton1.disabled=true
+       document.botones3.boton2.disabled=true
+        document.botones3.boton3.disabled=true
+         document.botones3.boton4.disabled=true
+}
+function one4(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
+    document.botones4.boton1.disabled=true
+       document.botones4.boton2.disabled=true
+        document.botones4.boton3.disabled=true
+         document.botones4.boton4.disabled=true
+}
+
+function two4(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
+   document.botones4.boton1.disabled=true
+       document.botones4.boton2.disabled=true
+        document.botones4.boton3.disabled=true
+         document.botones4.boton4.disabled=true
+}
+function one5(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
+    document.botones5.boton1.disabled=true
+       document.botones5.boton2.disabled=true
+        document.botones5.boton3.disabled=true
+         document.botones5.boton4.disabled=true
+}
+
+function two5(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
+   document.botones5.boton1.disabled=true
+       document.botones5.boton2.disabled=true
+        document.botones5.boton3.disabled=true
+         document.botones5.boton4.disabled=true
+}
+function one6(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
+    document.botones6.boton1.disabled=true
+       document.botones6.boton2.disabled=true
+        document.botones6.boton3.disabled=true
+         document.botones6.boton4.disabled=true
+}
+
+function two6(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
+   document.botones6.boton1.disabled=true
+       document.botones6.boton2.disabled=true
+        document.botones6.boton3.disabled=true
+         document.botones6.boton4.disabled=true
+}
+function one7(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
+    document.botones7.boton1.disabled=true
+       document.botones7.boton2.disabled=true
+        document.botones7.boton3.disabled=true
+         document.botones7.boton4.disabled=true
+}
+
+function two7(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
+   document.botones7.boton1.disabled=true
+       document.botones7.boton2.disabled=true
+        document.botones7.boton3.disabled=true
+         document.botones7.boton4.disabled=true
+}
+function one8(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
+    document.botones8.boton1.disabled=true
+       document.botones8.boton2.disabled=true
+        document.botones8.boton3.disabled=true
+         document.botones8.boton4.disabled=true
+}
+
+function two8(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
+   document.botones8.boton1.disabled=true
+       document.botones8.boton2.disabled=true
+        document.botones8.boton3.disabled=true
+         document.botones8.boton4.disabled=true
+}
+function one9(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
+    document.botones9.boton1.disabled=true
+       document.botones9.boton2.disabled=true
+        document.botones9.boton3.disabled=true
+         document.botones9.boton4.disabled=true
+}
+
+function two9(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
+   document.botones9.boton1.disabled=true
+       document.botones9.boton2.disabled=true
+        document.botones9.boton3.disabled=true
+         document.botones9.boton4.disabled=true
+}
+function one10(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
+    document.botones10.boton1.disabled=true
+       document.botones10.boton2.disabled=true
+        document.botones10.boton3.disabled=true
+         document.botones10.boton4.disabled=true
+}
+
+function two10(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
+   document.botones10.boton1.disabled=true
+       document.botones10.boton2.disabled=true
+        document.botones10.boton3.disabled=true
+         document.botones10.boton4.disabled=true
+}
+function final1(obj) 
+{
+    obj.style.backgroundColor="#3CB371";
+    obj.innerHTML="Correcto";
+      contador++;
+    alert("Felicitaciones has terminado tu puntuación final es: "+contador);
+    document.botones10.boton1.disabled=true
+       document.botones10.boton2.disabled=true
+        document.botones10.boton3.disabled=true
+         document.botones10.boton4.disabled=true
+}
+
+function final2(obj) 
+{
+   obj.style.backgroundColor="#B22222";
+   obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Felicitaciones has terminado, tu puntuación final es: "+contador)
+   document.botones10.boton1.disabled=true
+       document.botones10.boton2.disabled=true
+        document.botones10.boton3.disabled=true
+         document.botones10.boton4.disabled=true
+}
